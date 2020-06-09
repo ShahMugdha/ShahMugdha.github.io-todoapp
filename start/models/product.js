9e716ec77a9b1985331135026b61ac8a3956baee
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2020-06-09 11:13:23 +0530

    Commit-I:Bug Fixes.

--- start/models/product.js
+++ start/models/product.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
-const productSchema=mongoose.Schema({
-    _id:mongoose.Types.ObjectId,
-    name:String,
-    email:String
-})
-module.exports=mongoose.model('product', productSchema);
+const mongoose = require("mongoose");
+const productSchema = mongoose.Schema(
+  {
+    title: String,
+    content: String
+  },
+  {
+    writeConcern: {
+      w: "majority",
+      j: true,
+      wtimeout: 1000
+    }
+  }
+);
+module.exports = mongoose.model("product", productSchema);
--- start/routes/api/members.js
+++ start/routes/api/members.js
@@ -1,131 +1,98 @@
-const express = require('express');
-const uuid = require('uuid');
+const express = require("express");
+const uuid = require("uuid");
 const router = express.Router();
-const members = require('../../Members');
-const Product=require('../../models/product');
-const mongoose = require('mongoose');
+const members = require("../../Members");
+const Product = require("../../models/product");
+const mongoose = require("mongoose");
 
 // Gets All Members
-router.get('/', (req, res) => {
-  Product.find()
-  .exec()
-  .then(docs=>{
-    console.log(docs);
-    res.status(200).json(docs);
-  })
-  .catch(err=>console.log(err));
-  //res.json(members)
+router.get("/", (req, res) => {
+  Product.find({})
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => console.log(err));
 });
 
 // Get Single Member
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const found = members.some(member => member.id === parseInt(req.params.id));
-  const id=req.params.id;
+  const id = req.params.id;
   Product.findById(id)
-  .exec()
-  .then(doc=>{
-    console.log("from database",doc);
-    res.status(200).json(doc);
-  })
-  .catch(err=>console.log(err));
-
-  /*if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }*/
+    .exec()
+    .then(doc => {
+      console.log("from database", doc);
+      res.status(200).json(doc);
+    })
+    .catch(err => console.log(err));
 });
 
 // Create Member
-router.post('/', (req, res) => {
-  const newMember = {
-    id: uuid.v4(),
-    name: req.body.name,
-    email: req.body.email,
-    status: 'active'
-  };
-
-  const product=new Product({
-    _id:new mongoose.Types.ObjectId(),
-    name:req.body.name,
-    email:req.body.email
+router.post("/", (req, res) => {
+  const product = new Product({
+    title: req.body.title,
+    content: req.body.content
   });
 
-  product.save().then(result=>{
-    console.log(result);
-    res.status(201).json({
-      message:"handling post requests",
-      createdProduct:result
-    });
-  })
-  .catch(err=>console.log(err));
-  res.status(500).json({error:err})
-
-  if (!newMember.name || !newMember.email) {
-    return res.status(400).json({ msg: 'Please include a name and email' });
+  if (!product.title || !product.content) {
+    return res.status(400).json({ msg: "Please include a title and content" });
   }
 
-  members.push(newMember);
-  res.json(members);
-  //res.redirect('/');
-  
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "handling post requests",
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 // Update Member
-router.patch('/:id', (req, res) => {
-  const id=req.params.id;
-  const updateOps={};
-  for (const ops of req.body){
-    updateOps[ops.propName]=ops.value;
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ msg: "Please include a title and content" });
   }
-  Product.update({_id:id}, {$set:updateOps})
-  .exec()
-  .then(resullt=>{
-    console.log(result);
-    res.status(200).json(result);
-  })
-  .catch(err=>{
-    console.log(err);
-    res.status(500).json({error:err})
-  });
- /* const found = members.some(member => member.id === parseInt(req.params.id));
 
-  if (found) {
-    const updMember = req.body;
-    members.forEach(member => {
-      if (member.id === parseInt(req.params.id)) {
-        member.name = updMember.name ? updMember.name : member.name;
-        member.email = updMember.email ? updMember.email : member.email;
-
-        res.json({ msg: 'Member updated', member });
-      }
+  Product.findOneAndUpdate(
+    { _id: id },
+    { $set: { title, content } },
+    { new: true }
+  )
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }*/
 });
 
 // Delete Member
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const found = members.some(member => member.id === parseInt(req.params.id));
-  const id=req.params.id;
-  Product.remove({_id:id})
-  .exec()
-  .then(result=>{
-    res.status(200).json(result);
-  })
-  .catch(err=>{
-    console.log(err);
-    res.status(500).json({error:err});
-  });
-  /*if (found) {
-    res.json({
-      msg: 'Member deleted',
-      members: members.filter(member => member.id !== parseInt(req.params.id))
+  const id = req.params.id;
+  Product.findOneAndRemove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }*/
 });
 
 module.exports = router;
